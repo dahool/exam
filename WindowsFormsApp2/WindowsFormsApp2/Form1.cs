811ff8283f71e0bcd78c44ad3217a46927d9dd26
Gabriel <gabriel.sgt@gmail.com>
2018-05-14 19:56:58 -0300

    exam1

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using service;
+using models;
+
+namespace WindowsFormsApp2
+{
+    
+    public partial class Form1 : Form
+    {
+
+        private Building building;
+
+        public Form1()
+        {
+            InitializeComponent();
+            building = BuildingFactory.create();
+            building.ResultNotificationEvent += ResultFound;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();   
+            this.building.search(textBox1.Text);
+        }
+
+        private void ResultFound(object sender, Owner o)
+        {
+            listView1.Items.Add(o.name + ", " + o.lastName);
+        }
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Models.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace service
+{
+    using models;
+
+    public class BuildingFactory
+    {
+
+        private BuildingFactory()
+        {
+        }
+
+        public static Building create()
+        {
+            Owner o1 = new Owner("juan","teste");
+            Owner o2 = new Owner("jose","pato");
+            Owner o3 = new Owner("jose", "ganso");
+
+            Building b = Building.create();
+            b.addUnit(o1);
+            b.addUnit(o2);
+            b.addUnit(o3);
+            return b;
+        }
+
+    }
+}
+
+namespace models
+{
+
+    public delegate void OwnerFoundEventHandler(object sender, Owner o);
+
+    public delegate void ResultNotificationEventHandler(object sender, Owner o);
+
+    public class Building
+    {
+        private List<Unit> units = new List<Unit>();
+
+        public ResultNotificationEventHandler ResultNotificationEvent;
+
+        private Building()
+        {
+        }
+
+        public void search(String name)
+        {
+            foreach (Unit u in units)
+            {
+                u.search(name);
+            }
+        }
+
+        public void addUnit(Owner o)
+        {
+            Unit u = new Unit(this, o);
+            this.units.Add(u);
+        }
+
+        public static Building create()
+        {
+            return new Building();
+        }
+
+        public void OnFoundNotification(object sender, Owner o)
+        {
+            if (ResultNotificationEvent != null)
+            {
+                ResultNotificationEvent(this, o);
+            }
+        }
+    }
+
+    public class Unit
+    {
+
+        private List<Owner> owners = new List<Owner>();
+
+        private Building building;
+
+        public Unit(Building b, Owner o)
+        {
+            this.building = b;
+            addOwner(o);
+        }
+
+        public void addOwner(Owner o)
+        {
+            o.OwnerFoundEvent += this.building.OnFoundNotification;
+            this.owners.Add(o);
+        }
+
+        public void search(String name)
+        {
+            foreach (Owner o in owners)
+            {
+                o.validate(name);
+            }
+        }
+
+    }
+
+    public class Owner
+    {
+        public String name;
+
+        public String lastName;
+
+        public event OwnerFoundEventHandler OwnerFoundEvent;
+
+        public Owner(String name, String lastName)
+        {
+            this.name = name;
+            this.lastName = lastName;
+        }
+
+        public void validate(String name)
+        {
+            if (this.name == name)
+            {
+                onOwnerFound(this);
+            }
+        }
+
+        public void onOwnerFound(Owner o)
+        {
+            if (OwnerFoundEvent != null)
+            {
+                OwnerFoundEvent(this, o);
+            }
+        }
+
+    }
+
+}
+
